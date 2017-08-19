af689e3b1c98d24b9401d8ab49e6ca08eeda345c
Michael Pascuzzi <gatorpazz@gmail.com>
2017-08-19 14:34:02 -0400

    Firebase initialization

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import { View, Text } from 'react-native';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyA8Mdkdc_RgynpDxc0kw2THsokXYScCaU0',
+      authDomain: 'reactnativeauth-c3650.firebaseapp.com',
+      databaseURL: 'https://reactnativeauth-c3650.firebaseio.com',
+      projectId: 'reactnativeauth-c3650',
+      storageBucket: 'reactnativeauth-c3650.appspot.com',
+      messagingSenderId: '894517053625'
+    });
+  }
   render() {
     return (
       <View>
